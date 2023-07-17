994495cd810fb40552fec879704ea46edecc9a20
VishalWarhade <115239850+VishalWarhade@users.noreply.github.com>
2023-07-17 19:28:56 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,13 +3,18 @@ pipeline {
     stages {
         stage ('pull') {
             steps{
-                git branch: 'main', url: 'https://github.com/VishalWarhade/project.git'
+                git 'https://github.com/VishalWarhade/project.git'
             }
         }
         
         stage ('deploy') {
             steps {
-                echo "deploy success"
+                sh 'mv /home/centos/workspace/job1/index.html /usr/share/nginx/html/index.html'
+            }
+        }
+        stage ('restart nginx') {
+            steps{
+                sh 'sudo systemctl restart nginx'
             }
         }
 
