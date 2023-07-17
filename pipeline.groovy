9a3b7b6f4bd1f5bdb2e14b3fa918f9dd7237cb56
VishalWarhade <115239850+VishalWarhade@users.noreply.github.com>
2023-07-17 19:43:06 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('pull') {
             steps{
-                git 'https://github.com/VishalWarhade/project.git'
+                git branch: 'main', url: 'https://github.com/VishalWarhade/project.git'
             }
         }
         
